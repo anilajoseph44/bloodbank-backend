6d5ad870c6c54b0eeed65f0a386fcc3f663b8760
anilajoseph44 <anilasandrajoseph@gmail.com>
2024-02-08 21:47:25 +0530

    done

---
+++ controllers/userRouter.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const usermodel=require("../models/usermodel")
+
+const router=express.Router()
+
+router.post("/add",async(req,res)=>{
+
+    let data=req.body
+    let user=new usermodel(data)
+    let response=await user.save()
+    res.json(
+        {
+            status:"success"
+        }
+    )
+
+})
+
+module.exports=router
