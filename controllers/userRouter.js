0e5eff7d6d06671e4ccc6c59dedfcca92bf262d1
anilajoseph44 <anilasandrajoseph@gmail.com>
2024-02-08 22:43:22 +0530

    done

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -22,4 +22,12 @@ router.get("/view",async(req,res)=>{
 
 })
 
+router.post("/search",async(req,res)=>{
+
+    let input=req.body
+    let data=await usermodel.find(input)
+    res.json(data)
+
+})
+
 module.exports=router
